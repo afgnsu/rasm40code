a7c04e8b03ef523b711039fa4239435705e55558
Bryan Bibat <bry@bryanbibat.net>
2014-06-02 01:42:43 +0800

    Add Remarks field to Debt Controller and View

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -69,6 +69,6 @@ class DebtsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def debt_params
-      params.require(:debt).permit(:name, :item, :amount)
+      params.require(:debt).permit(:name, :item, :amount, :remarks)
     end
 end
