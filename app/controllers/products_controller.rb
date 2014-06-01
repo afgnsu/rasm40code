857fbdd95401612111c2a39cd18b8cbdacb029ed
Bryan Bibat <bry@bryanbibat.net>
2014-06-02 03:35:16 +0800

    Initial code for Create Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,9 +6,22 @@ class ProductsController < ApplicationController
   def search
     @products = Product.find_all_by_name(params[:name])
   end
-  
+
   def show
     @product = Product.find(params[:id])
+  end
+
+  def new
+    # create a dummy Product
+  end
+  
+  def create
+    # create a Product based on submitted form
+    # save Product
+    # if save is successful
+    #   show new Product
+    # else 
+    #   go back to current page and display error
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ AlingnenaApp::Application.routes.draw do
   resources :debts
 
   get 'products' => 'products#index', :as => 'products'
+  post 'products' => 'products#create'
+  get 'products/new' => 'products#new', :as => 'new_product' 
   get 'products/search' => 'products#search', :as => 'search_products'
   get 'products/:id' => 'products#show', :as => 'product'
   delete 'products/:id' => 'products#destroy'
