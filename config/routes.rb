32b24f96a02aa68acf02315f06729d3afd2a9eb8
Bryan Bibat <bry@bryanbibat.net>
2014-06-02 02:58:57 +0800

    Add Route names

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 AlingnenaApp::Application.routes.draw do
   resources :debts
 
-  get 'products' => 'products#index' 
-  get 'products/:id' => 'products#show'
-  
+  get 'products' => 'products#index', :as => 'products' 
+  get 'products/:id' => 'products#show', :as => 'product'
+
 end
