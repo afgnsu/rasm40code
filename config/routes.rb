4eec83249f7b52076040d59993a082165999b7f5
Bryan Bibat <bry@bryanbibat.net>
2014-06-02 03:26:13 +0800

    Initial code for Search Product

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 AlingnenaApp::Application.routes.draw do
   resources :debts
 
-  get 'products' => 'products#index', :as => 'products' 
+  get 'products' => 'products#index', :as => 'products'
+  get 'products/search' => 'products#search', :as => 'search_products'
   get 'products/:id' => 'products#show', :as => 'product'
   delete 'products/:id' => 'products#destroy'
 end
