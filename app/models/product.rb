4e7e500de73849ec5cf2aced54f5783981c3f91d
Bryan Bibat <bry@bryanbibat.net>
2014-06-02 17:18:08 +0800

    Has many through

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
   has_many :line_items
+  has_many :purchases, through: :line_items
 
   before_validation :assign_default_description
 
