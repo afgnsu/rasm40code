dcaf07d43de79e00ee59dd770e395121acd0ae39
Bryan Bibat <bry@bryanbibat.net>
2014-06-02 17:20:53 +0800

    Enable Eager-loading

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -64,7 +64,7 @@ class PurchasesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_purchase
-      @purchase = Purchase.find(params[:id])
+      @purchase = Purchase.includes(line_items: :product).find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
