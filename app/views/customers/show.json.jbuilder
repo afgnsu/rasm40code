31026cd603164928f82da600d1b09563b3f2dbcb
Bryan Bibat <bry@bryanbibat.net>
2014-06-02 21:24:50 +0800

    Customer scaffold and initial spec run

---
+++ app/views/customers/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @customer, :id, :name, :active, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602103300) do
+ActiveRecord::Schema.define(version: 20140602132319) do
+
+  create_table "customers", force: true do |t|
+    t.string   "name"
+    t.boolean  "active"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "debts", force: true do |t|
     t.string   "name"
